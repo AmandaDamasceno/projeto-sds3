8f9f3e3a8e97d342197ef05f86ee57f349465553
Amanda Damasceno <amandamasceno.rocha@gmail.com>
2021-09-08 17:08:16 -0300

    Security config

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,10 +4,11 @@ const Footer = () => {
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/Amanda-MDR" target="_blank" rel="noreferrer">Amanda Damasceno</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-       Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
         </footer>
     );
+   
 }
 
 export default Footer;
